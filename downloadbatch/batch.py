5c1255b4a7c345f6869473aa01b7298ca0f6ec40
chateauxvt <tolgaoa@vt.edu>
2023-02-04 20:56:13 -0500

    fixed bug in the batch query collection script which was causing traceIDs to be cut mid-way for select traces

---
+++ downloadbatch/batch.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import urllib3, shutil
+
+
+soup = BeautifulSoup(open("./Jaeger_UI2.html"), "html.parser")
+
+tags = soup.find_all('a', {'class': 'ResultItemTitle--item ub-flex-auto'})
+
+
+def download_json(url):
+    searcht = "trace/"
+    tracei = url.rindex(searcht)    
+    crop = tracei - len(url) + len(searcht)
+    json_url = 'http://10.42.1.8:16686/api/traces/' + url[crop::] + '?prettyPrint=true'
+    http = urllib3.PoolManager()
+
+    path = url[crop::] + '.json'
+
+    with http.request('GET', json_url, preload_content=False) as r, open(path, 'wb') as out_file:
+        shutil.copyfileobj(r, out_file)
+
+for tag in tags:
+    link = tag.get('href')
+    download_json(link)
